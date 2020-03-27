c3b7dc2e1492b3abe7e50dd1d8a12770aaa4dce6
Robinson Junior <robinson.juniorr@gmail.com>
2020-03-26 23:37:58 -0300

    connect with backend

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 
 import "./styles.css";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 import heroesImg from "../../assets/heroes.png";
 
 export default function Login() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post("sessions", { id });
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+      history.push("/profile");
+    } catch (error) {
+      alert("ID inválido.");
+    }
+  }
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            onChange={e => setId(e.target.value)}
+            value={id}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
